6e9e8412d2873740e6f3a6e4094f62c12fac4409
Cytotoxicity <cytotoxicity8@naver.com>
2020-12-22 23:46:55 +0900

    실 데이터 적용 중

--- Scripts/R code.R
+++ Scripts/R code.R
@@ -1,14 +1,29 @@
-pacman::p_load(readxl, reticulate)
+pacman::p_load(readxl, reticulate, xgboost)
+
+install.packages("reticulate")
+
+reticulate::repl_python()
+
+use_condaenv("r-reticulate")
+conda_list()
+use_python()
+use_python("C:\\Users\\kxusj\\Documents\\.virtualenvs\\r-reticulate\\python.exe")
+Sys.which("python")
 
 py_install("xgboost", pip=T) #설치가 안 되면 pip=T 로 바꿔놓자!
-py_install("lightgbm")
+py_install("lightgbm", pip=T)
 py_install("sklearn", pip=T)
+py_install("numpy", pip=T) 
+py_install("pycaret", pip=T)
+
+conda_create()
+conda_list()
+conda_install(envname="r-reticulate", packages="pycaret")
+conda_remove("r-reticulate")
 
 conda_install("r-reticulate", "xgboost")
 
-data <- read.csv("Data/credit.csv")
-
-data_py <- r_to_py(data, convert=T)
+data <- read.csv("Data/titanic_train.csv")
 
 data_py
 
@@ -23,3 +38,6 @@ virtualenv_list()
 
 xgboost <- import("xgboost")
 scipy <- import("scipy")
+
+#xgboost 호환
+
--- Scripts/python code.py
+++ Scripts/python code.py
@@ -1,13 +1,12 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+import lightgbm
+from pycaret.classification import * 
 
-mat = np.zeros((2, 3))
-
-mat.transpose()
-
-data_py
-
-data_py = r.data
-
-type(pd.DataFrame(data_py))
+clf = setup(data = r.train, target = 'voted')
+best_3 = compare_models(sort = 'AUC', n_select = 3, verbose=False)Z
+compare_models()
+blended = blend_models(estimator_list = best_3, fold = 5, method = 'soft')
+pred_holdout = predict_model(blended)
+Z
